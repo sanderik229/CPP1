8bcd5c07df851d446a5601ef73796c6c8f048ec9
sanderik229 <144127176+sanderik229@users.noreply.github.com>
2024-01-15 17:20:24 +0300

    123213

---
+++ 123.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    cout << "Ярко светит елочка,\nПраздника все ждут.\nДед Мороз с Снегурочкой\nВ гости к нам идут.";
+}
